036e30ee79d38cd4c2f651f572649cfc4eaeccba
Cristian Scapin <cristian.scapin654@icloud.com>
2025-11-29 10:22:39 +0100

    destination-city optimal

--- 1436-destination-city/destination-city.cpp
+++ 1436-destination-city/destination-city.cpp
@@ -1,58 +1,45 @@
 #include <algorithm>
 #include <cassert>
 #include <iostream>
-#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    string destCity(vector<vector<string>>& paths) {
-      unordered_map<string, int> city_conns;
-      
-      for (const auto &path: paths) {
-	const auto from = path[0];
-	const auto to = path[1];
-	
-	
-	// initialize at 1 if doesnt already exist or add 1
-	if (const auto &it = city_conns.find(from); it == city_conns.end()) {
-	  city_conns[from] = 1;
-	} else {
-	  city_conns[from]++;
-	}
+  string destCity(vector<vector<string>> &paths) {
+    unordered_set<string> from_cities;
 
-	// if doesnt exist initialize at 0, do nothing otherwise
-	if (const auto &it = city_conns.find(to); it == city_conns.end()) {
-	  city_conns[to] = 0;
-	}
-      }
-      
-      for (const auto &pair: city_conns) {
-	if (pair.second == 0) return pair.first; 
-      }
-      
-      assert(false && "It should not reach here");
+    for (const auto &path : paths) {
+      from_cities.insert(path[0]);
     }
-};
 
+    for (const auto &path : paths) {
+      if (from_cities.find(path[1]) == from_cities.end())
+        return path[1];
+    }
+
+    assert(false && "It should not reach here");
+  }
+};
 
 int main() {
   Solution s;
 
   {
-    std::vector<std::vector<string>> arr = {{"London","New York"},{"New York","Lima"},{"Lima","Sao Paulo"}};
+    std::vector<std::vector<string>> arr = {
+        {"London", "New York"}, {"New York", "Lima"}, {"Lima", "Sao Paulo"}};
     assert(s.destCity(arr) == "Sao Paulo" && "Testcase 1");
   }
 
   {
-    std::vector<std::vector<string>> arr = {{"B","C"},{"D","B"},{"C","A"}};
+    std::vector<std::vector<string>> arr = {{"B", "C"}, {"D", "B"}, {"C", "A"}};
     assert(s.destCity(arr) == "A" && "Testcase 2");
   }
 
   {
-    std::vector<std::vector<string>> arr = {{"A","Z"}};
+    std::vector<std::vector<string>> arr = {{"A", "Z"}};
     assert(s.destCity(arr) == "Z" && "Testcase 3");
   }
 }
